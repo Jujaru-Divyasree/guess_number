78b7f9ca9122f433b5fcc98e37bb7cb132fff857
jujaru Divyasree <divyasreejujaru8@gmail.com>
2026-04-07 11:26:37 +0530

    commit in local vs code

--- guess_number.py
+++ guess_number.py
@@ -4,6 +4,7 @@ print ('welcome to guess number game !\nyou have 5 chances to guess the  number'
 num = random.randint(1, 50)
 chance = 5
 gc = 0
+i=0
 while chance > gc:
     gc +=1
     guess = int(input('guess the number between 1 and 50 : '))
@@ -18,6 +19,5 @@ while chance > gc:
         print ('too high ')
         """ elif chance == gc and guess !=num :
         print ('sorry you lose ! the number is : ',num)       """
-for (i in chance ):
+for (i) in range(1, chance + 1) :
     print(i)
-
